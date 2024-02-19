5cdeb12afb866c6a9508e82fdae346dbfd49cb57
Qianclear <1799089237@qq.com>
2024-02-19 17:49:56 +0800

    添加项目文件。

---
+++ WpfDeskTop/DeskWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms.Integration;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Vlc.DotNet.Forms;
+
+namespace WpfDeskTop
+{
+    /// <summary>
+    /// DeskWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class DeskWindow : Window
+    {
+        //由于可能存在多个屏幕，故可能需要多次创建
+        public List<VlcControl> controlsList = new List<VlcControl>();
+        public List<WindowsFormsHost> windowsFormsList = new List<WindowsFormsHost>();
+
+        public string[] options = new string[]
+        {
+            "--input-repeat=65535"
+        };
+
+        public DeskWindow()
+        {
+            InitializeComponent();
+        }
+
+        //添加组件  参数：偏移量，宽，高
+        public void AddDisplayComponent(int offset, int width, int height)
+        {
+            DockPanel dockPanel = new DockPanel();//添加dockPannel组件
+            dockPanel.Margin = new Thickness(offset, 0, 0, 0); //属性设置
+            dockPanel.HorizontalAlignment = HorizontalAlignment.Left;
+            dockPanel.Visibility = Visibility.Collapsed;
+            dockPanel.Width = width;
+            dockPanel.Height = height;
+
+            WindowsFormsHost windowsFormsHost = new WindowsFormsHost();
+            VlcControl vlcControl = new VlcControl();
+            //初始化VLC
+            DirectoryInfo directoryInfo = new DirectoryInfo(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"libvlc",IntPtr.Size == 4?"win-x86":"win-x64"));
+            vlcControl.BeginInit();
+            vlcControl.VlcLibDirectory = directoryInfo;//指定初始化库的位置
+            //字符串列表的形式存储参数，故在上面
+            vlcControl.VlcMediaplayerOptions = options;
+            vlcControl.EndInit();
+
+            //组件放置嵌套，一层一层
+            windowsFormsHost.Child = vlcControl;
+            dockPanel.Children.Add(windowsFormsHost);
+            rootGird.Children.Add(dockPanel);
+
+            //组件创建完毕之后加入列表
+            controlsList.Add(vlcControl);
+            windowsFormsList.Add(windowsFormsHost);
+        }
+
+        //播放
+        public void PlayDeskBg(string furl)
+        {
+            //由于之前设置的组件是不可见的，故第一步是先显示出来
+            //先找到所有DockPanel然后遍历显示
+            IEnumerable<DockPanel> enumerable = rootGird.Children.OfType<DockPanel>();
+            enumerable.ToList().ForEach((item) => { item.Visibility = Visibility.Visible; });
+            foreach(VlcControl vlc in controlsList)
+            {
+                vlc.Audio.Volume = 0;
+                vlc.Play(new Uri( furl));
+            }
+        }
+
+        //释放资源
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            foreach(VlcControl vlc in controlsList) {  vlc.Dispose(); }
+            foreach(WindowsFormsHost host in windowsFormsList) {  host.Dispose(); }
+        }
+    }
+}
---
+++ WpfDeskTop/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDeskTop
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public DeskWindow deskwindow = null;//创建DeskWindow
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StopWindow_Click(object sender, RoutedEventArgs e)
+        {
+            deskwindow.Close();
+            deskwindow = null;
+        }
+
+        private void ChoiceDeskBg_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            //添加筛选措施
+            openFileDialog.Filter = "(视频)|*.mp4;*.mkv;*.avi";
+            openFileDialog.Title = "选择视频";
+            bool? v = openFileDialog.ShowDialog();
+            string filename = "";//文件名
+            //判断是否选择了文件
+            if (v == true) { filename = openFileDialog.FileName; }
+            else { return; }
+
+            if(deskwindow == null)
+            {
+                //先创建对象
+                deskwindow = new DeskWindow();
+                //添加组件
+                //获取全部屏幕据
+                Screen[] allScreens = System.Windows.Forms.Screen.AllScreens;
+                List<Screen> screens = new List<Screen>(allScreens);
+                //将屏幕按照X坐标进行排序
+                screens.Sort(delegate(System.Windows.Forms.Screen s1,System.Windows.Forms.Screen s2)
+                {
+                    return s1.Bounds.X.CompareTo(s2.Bounds.X);
+                });
+
+                //按顺序添加组件
+                int offset = 0; //初始化偏移量为0
+                foreach (Screen screen in screens)
+                {
+                    deskwindow.AddDisplayComponent(offset, screen.Bounds.Width, screen.Bounds.Height);
+                    offset += screen.Bounds.Width;
+                }
+                deskwindow.Width = offset;
+                deskwindow.Height = SystemParameters.PrimaryScreenHeight;
+                deskwindow.PlayDeskBg(filename);
+                deskwindow.Show();
+            }
+            else
+            {
+                deskwindow.PlayDeskBg(filename);
+            }
+            Tools.SetDeskTop(deskwindow);
+        }
+    }
+}
